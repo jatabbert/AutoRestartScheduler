c1de4c336c464b9e7b8439f54c0bbfdb87afb2da
jatabbert <jeffreytabbert@gmail.com>
2025-01-10 11:10:04 -0600

    Update RestartDropDownMenu.ps1
    Changed cutoff date/message to Saturday.

--- RestartDropDownMenu.ps1
+++ RestartDropDownMenu.ps1
@@ -1,4 +1,3 @@
-
 Add-Type -AssemblyName System.Windows.Forms
 
 # Create a new form
@@ -7,8 +6,9 @@ $form.Text = "Restart Time Selection"
 $form.Size = New-Object System.Drawing.Size(670, 240)
 $form.StartPosition = "CenterScreen"
 $form.FormBorderStyle = "FixedDialog"
-$form.ControlBox = $false  # Hide the control box
+$form.ControlBox = $false  # Hide the control box so they can't minimize/close
 $form.TopMost = $true # forces popup to stay on top of everything else
+$form.ShowInTaskbar = $false # hide this popup from the windows taskbar
 
 # Create labels
 $label = New-Object System.Windows.Forms.Label
@@ -16,10 +16,10 @@ $label.Location = New-Object System.Drawing.Point(10, 10)
 $label.Size = New-Object System.Drawing.Size(660, 100)
 $label.Font = New-Object System.Drawing.Font('Times New Roman',13)
 $label.Text = "Windows Updates requires a restart of your computer to finish.
-If you do not select a time your computer will automatically restart Friday at 5PM.
+If you do not select a time your computer will automatically restart Saturday at 5PM.
 If your computer is sleeping at the scheduled time, it WILL restart as soon as it wakes up.
 
-Select a time, between now and Friday at 5PM, that you want to schedule your restart:"
+Select a time, between now and Saturday at 5PM, that you want to schedule your restart:"
 $form.Controls.Add($label)
 
 # Populate a hash table used to fill in Day dropdown and for later date string building
@@ -29,7 +29,7 @@ do {
     $fullDay = (Get-Date).AddDays($increment).ToString("dddd, MMMM dd, yyyy")
     $dayTable.Add($increment, $fullDay)
     $increment++
-} while ($fullDay -notlike "Friday*")
+} while ($fullDay -notlike "Saturday*")
 
 # Create Day dropdown
 $dayDropDown = New-Object System.Windows.Forms.ComboBox
@@ -38,7 +38,7 @@ $dayDropDown.Size = New-Object System.Drawing.Size(160, 20)
 for ($i = 0; $i -lt $dayTable.Count; $i++) {
     $retrieved = $dayTable[$i]
     $retrieved = $retrieved.Substring(0, $retrieved.Length - 6)
-    $garbage = $dayDropDown.Items.Add($retrieved)
+    $dayDropDown.Items.Add($retrieved) | Out-Null
 }
 $dayDropDown.SelectedIndex = 0
 $dayDropDown.DropDownHeight = 100
@@ -58,7 +58,7 @@ $hourDropDown.Location = New-Object System.Drawing.Point(200, 125)
 $hourDropDown.Size = New-Object System.Drawing.Size(40, 20)
 $hours = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
 foreach ($hour in $hours) {
-    $garbage = $hourDropDown.Items.Add($hour)
+    $hourDropDown.Items.Add($hour) | Out-Null
 }
 $hourDropDown.SelectedIndex = 4
 $hourDropDown.DropDownHeight = 300
@@ -70,7 +70,7 @@ $minuteDropDown.Location = New-Object System.Drawing.Point(245, 125)
 $minuteDropDown.Size = New-Object System.Drawing.Size(40, 20)
 $minutes = ("00", "05", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55")
 foreach ($minute in $minutes) {
-    $garbage = $minuteDropDown.Items.Add($minute)
+    $minuteDropDown.Items.Add($minute) | Out-Null
 }
 $minuteDropDown.SelectedIndex = 0
 $minuteDropDown.DropDownHeight = 300
@@ -80,8 +80,8 @@ $form.Controls.Add($minuteDropDown)
 $ampmDropDown = New-Object System.Windows.Forms.ComboBox
 $ampmDropDown.Location = New-Object System.Drawing.Point(290, 125)
 $ampmDropDown.Size = New-Object System.Drawing.Size(40, 20)
-$garbage = $ampmDropDown.Items.Add("AM")
-$garbage = $ampmDropDown.Items.Add("PM")
+$ampmDropDown.Items.Add("AM") | Out-Null
+$ampmDropDown.Items.Add("PM") | Out-Null
 $ampmDropDown.SelectedIndex = 1
 $form.Controls.Add($ampmDropDown)
 
@@ -103,13 +103,13 @@ $form.Controls.Add($YesButton)
 
 $form.AcceptButton = $okButton
 
-# get the datetime for this Friday at 5pm
+# get the datetime for this upcoming cutoffday at X time
 $incrementer = 0
 do {
     $friday = (Get-Date).AddDays($incrementer).ToString("D")
     $incrementer++
 }
-while ($friday -notlike "Friday*")
+while ($friday -notlike "Saturday*")
 
 $friday = $friday + " 5:00 PM"
 [datetime]$theDate = Get-Date -Date $friday
